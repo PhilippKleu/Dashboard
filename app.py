d2041d8296266cd3222794f80d4691abb0155df3
PhilippKleu <p.kleusberg28@gmail.com>
2025-05-23 11:21:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -885,12 +885,14 @@ with tab1:
                 color_levels = 10
                 max_vertices_for_density = 250
         
-                techs_with_time_data = [tech for tech in tech_time_map if len(tech_time_map[tech]) > 1]
-                n_techs = len(techs_with_time_data)
-                n_cols = 2
-                n_rows = ceil(n_techs / n_cols)
+                n_techs = sum(1 for v in tech_time_map.values() if len(v) >= 1)
+                n_rows = ceil(n_techs / st.session_state.get("n_cols_plots", 3))
+                plot_width_per_col = 6
+                plot_height_per_row = 3.5
+                fig_width = plot_width_per_col * st.session_state.get("n_cols_plots", 3)
+                fig_height = plot_height_per_row * n_rows
         
-                fig_dichte, axs = plt.subplots(n_rows, n_cols, figsize=(13, 4 * n_rows))
+                fig_dichte, axs = plt.subplots(n_rows,st.session_state.get("n_cols_plots", 3), figsize=(fig_width, fig_height))
                 axs = axs.flatten()
                 fig_dichte.patch.set_facecolor('#f4f4f4')
         
