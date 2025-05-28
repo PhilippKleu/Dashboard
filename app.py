628f3a83ead376de079e934817a0e87fca2525f0
PhilippKleu <p.kleusberg28@gmail.com>
2025-05-28 16:30:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -368,8 +368,8 @@ with tab1:
             filtered_data = pd.DataFrame()
             convex_data = pd.DataFrame()
         
-            filtered_cols = [MAA_PREFIX + var for var in ordered_vars if var in technologies]
-            selected_data = tech_data[filtered_cols] if ordered_techs else pd.DataFrame(index=tech_data.index)
+            selected_data = tech_data[filtered_cols] if any(var in technologies for var in ordered_vars) else pd.DataFrame(index=tech_data.index)
+            
             current_indices = selected_data.index if ordered_techs else tech_data.index
         
             # === Slider-Filter anwenden ===
