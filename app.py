c3437a2949eaa406f6e759abfa25b8bfbb4cc596
PhilippKleu <p.kleusberg28@gmail.com>
2025-05-28 14:51:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1797,7 +1797,7 @@ with tab1:
                 st.dataframe(convex_with_all, use_container_width=True)
 
 with tab2:
-    st.title("📘 Explanation")
+    st.markdown("## Layout Overview")
 
     # =====================
     # SECTION: Overview
