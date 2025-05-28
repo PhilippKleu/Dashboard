05e83d1725f6e2f252d1d48aa8615dda55fcf79d
PhilippKleu <p.kleusberg28@gmail.com>
2025-05-28 14:33:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1797,17 +1797,18 @@ with tab1:
                 st.dataframe(convex_with_all, use_container_width=True)
 
 with tab2: 
+    st.image(
+        "https://raw.githubusercontent.com/PhilippKleu/Dashboard/develope/Screenshot%202025-05-28%20152925.png",
+        caption="Screenshot der aktuellen Version (Branch: develope)",
+        use_column_width=True
+    )
          """
     ### 🧠 Technology Decision Tool – Overview & Usage
 
     This tool supports the exploration and evaluation of **technology transition pathways**. It enables interactive filtering, visualization, and generation of new combinations. The main features include:
     
     ---
-    st.image(
-        "https://raw.githubusercontent.com/PhilippKleu/Dashboard/develope/Screenshot%202025-05-28%20152925.png",
-        caption="Screenshot der aktuellen Version (Branch: develope)",
-        use_column_width=True
-    )
+    
     #### 🔹 1. Selection & Filtering
     - Choose any number of technologies from the dataset.
     - For each selected technology, a slider will appear to restrict its value range.
