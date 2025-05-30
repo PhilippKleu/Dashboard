01c5d0b1b530660749cf988c02996a7a218c852b
PhilippKleu <p.kleusberg28@gmail.com>
2025-05-30 09:34:25 +0200

    Update app_add.py

--- app_add.py
+++ app_add.py
@@ -1845,7 +1845,7 @@ with tab2:
     st.markdown("This tool allows users to interactively explore, analyze, and export data-driven decisions.")
     st.markdown("""
     <div style="border: 2px solid #ccc; padding: 10px; border-radius: 10px; background-color: #f9f9f9; width: 600px; margin: auto;">
-        <img src="https://raw.githubusercontent.com/PhilippKleu/Dashboard/develope/Screenshot%202025-05-28%20152925.png" width="100%">
+        <img src="https://raw.githubusercontent.com/PhilippKleu/Dashboard/develope/Bild1.png" width="100%">
         <p style="text-align: center; font-style: italic; margin-top: 5px;">Main dashboard interface</p>
     </div>
     """, unsafe_allow_html=True)
