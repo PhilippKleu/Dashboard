e5b7d685e2620faf36cd0289cbb74062a3a78178
PhilippKleu <p.kleusberg28@gmail.com>
2025-06-11 15:47:03 +0200

    Update app_add_plotly.py

--- app_add_plotly.py
+++ app_add_plotly.py
@@ -876,7 +876,7 @@ with tab1:
                 plot_indices = current_indices
                 
             if st.session_state.get("plot_type_selector2") == "Line Plot":
-                 plot_indices = vertex_df.loc[current_indices].sample(
+                plot_indices = vertex_df.loc[current_indices].sample(
                     n=st.session_state["max_plot_vertices"], replace=False, random_state=42
                 ).index
                 # 1. Gültige Technologien ermitteln
