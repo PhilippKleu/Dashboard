f62f6ada7505372f83c1435dddb2ae691a3ccab3
PhilippKleu <p.kleusberg28@gmail.com>
2025-06-12 15:12:25 +0200

    Update app_add_plotly.py

--- app_add_plotly.py
+++ app_add_plotly.py
@@ -886,7 +886,7 @@ with tab1:
                     "subplot_height_multiplier": 1.0,
                 
                     "horizontal_spacing": 0.08,
-                    "vertical_spacing": 0.2,
+                    "vertical_spacing": 0.11,
                 
                     "main_color": '#f4f4f4',
                     "dim_color": '#f4f4f4',
