d14b1e46310d089660215939eaf292984a829071
PhilippKleu <p.kleusberg28@gmail.com>
2025-07-15 09:58:55 +0200

    Update app_add_plotly.py

--- app_add_plotly.py
+++ app_add_plotly.py
@@ -378,7 +378,9 @@ def plot_operational_variables_over_time(
             ), row=row, col=col)
 
         # ✅ Konvexe Kombinationen (korrekter Spaltenabgleich)
+        st.write("###")
         st.write(filtered_convex_data)
+        st.write("###")
         st.write(st_convex)
         if show_convex and not st_convex.empty:
             if len(years) == 1:
