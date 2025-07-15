741e7767175608ebacac3cea7d80dc1aee1ea141
PhilippKleu <p.kleusberg28@gmail.com>
2025-07-15 10:18:05 +0200

    Update app_add_plotly.py

--- app_add_plotly.py
+++ app_add_plotly.py
@@ -378,21 +378,15 @@ def plot_operational_variables_over_time(
             ), row=row, col=col)
 
         # ✅ Konvexe Kombinationen (korrekter Spaltenabgleich)
-        st.write("NEW")
-        st.write("Filtered:")
-        st.write(filtered_convex_data)
-        st.write("Convex")
-        st.write(st_convex)
-        st.write(show_convex)
-        st.write(st_convex.empty)
+        
         if show_convex and not st_convex.empty:
             if len(years) == 1:
-                st.write("hier1")
+                
                 convex_col = f"{INSTALLED_CAPACITY_PREFIX}{tech}_{years[0]}"
-                st.write(convex_cols)
+                
                 if convex_col in filtered_convex_data.columns:
                     convex_vals = filtered_convex_data[convex_col].dropna()
-                    fig.add_trace(go.Scatter(
+                    fig_val.add_trace(go.Scatter(
                         x=[years[0]] * len(convex_vals),
                         y=convex_vals,
                         mode='markers',
@@ -401,14 +395,14 @@ def plot_operational_variables_over_time(
                         showlegend=False
                     ), row=row, col=col)
             else:
-                st.write("hier2")
+               
                 convex_cols = [f"{INSTALLED_CAPACITY_PREFIX}{tech}_{year}" for year in years]
-                st.write(convex_cols)
+                
                 if all(col in filtered_convex_data.columns for col in convex_cols):
                     for idx in filtered_convex_data.index:
                         values = filtered_convex_data.loc[idx, convex_cols].values
                         if not np.isnan(values).all():
-                            fig.add_trace(go.Scatter(
+                            fig_val.add_trace(go.Scatter(
                                 x=years,
                                 y=values,
                                 mode='lines',
