f563abe42423d73b7a2492200f1fe8980cb7f861
PhilippKleu <p.kleusberg28@gmail.com>
2025-08-19 09:42:55 +0200

    Update app_add_plotly.py

--- app_add_plotly.py
+++ app_add_plotly.py
@@ -645,6 +645,7 @@ def build_cols_from_time_map(time_map, techs, MAA_PREFIX):
     for tech in techs:
         year_cols = time_map.get(tech, [])
         years_cols_sorted = sorted(year_cols, key=lambda x: x[0])
+        st.write(years_cols_sorted)
         
         if MAA_PREFIX == "VALUE_":
             cols.extend([c for _, c in years_cols_sorted
