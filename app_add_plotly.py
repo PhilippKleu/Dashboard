bdc6c64bfd6ceac3817bf42322ba4b8362166301
PhilippKleu <p.kleusberg28@gmail.com>
2025-08-19 09:33:59 +0200

    Update app_add_plotly.py

--- app_add_plotly.py
+++ app_add_plotly.py
@@ -1396,6 +1396,7 @@ with tab1:
             
             # === Initiale Spaltenauswahl für KMeans
             if MAA_PREFIX == "VALUE_":
+                st.write("hier")
                 cols = build_cols_from_time_map(extract_time_series_map(vertex_df, MAA_PREFIX, mode="installed"), valid_techs_value, MAA_PREFIX)
             else:
                 st.write(valid_techs_value)
