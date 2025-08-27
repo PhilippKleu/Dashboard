3bc63d1cf329e16838b53565b5cb7d3ff7a9b356
PhilippKleu <p.kleusberg28@gmail.com>
2025-08-27 11:25:44 +0200

    Update app_add_plotly.py

--- app_add_plotly.py
+++ app_add_plotly.py
@@ -612,7 +612,7 @@ def plot_operational_variables_over_time(
     plot_title="Operational Variables Over Time",
     # Feste Abstände zwischen Subplots:
     h_gap=0.1,                # horizontal
-    v_gap=0.1,                # vertikal
+    v_gap=0.08,                # vertikal
 ):
     """
     Zeichnet ALLE gewünschten Plots als einzelne Subplots in ein Grid mit n_cols_val Spalten.
